88239982f17c9f2701f4035efa95aa733874e46c
Sankalp Sharma <sankalpsharma250@gmail.com>
2024-09-26 02:19:26 +0530

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true, // Optional, enables React's strict mode
-    swcMinify: true,       // Optional, enables SWC-based minifier for faster builds
-    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'mdx'], // Allows Next.js to recognize these extensions in the app directory
-  };
-  
-  export default nextConfig;
+  reactStrictMode: true, // Optional, enables React's strict mode
+  swcMinify: true,       // Optional, enables SWC-based minifier for faster builds
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'mdx'], // Allows Next.js to recognize these extensions in the app directory
+  output: 'export',      // Enables static export for the app
+  trailingSlash: true,   // Optional, adds trailing slashes to all paths for better compatibility with static hosting
+  images: {
+    unoptimized: true,   // Optional, disable image optimization, which is not compatible with static exports
+  },
+};
+
+export default nextConfig;
+
   
